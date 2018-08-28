76a44ea782345d5fd21773ca028b1dfb71ef2509
Miguel <miguelcudaihl@gmail.com>
2018-08-29 07:26:11 +0800

    added chapter 1 exercises

---
+++ Chapter.01/QuickSort.cs/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Horology;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+namespace QuickSort.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BenchmarkRunner.Run<QuickSortRunner>();
+            var x = new QuickSortRunner();
+            var sw = new Stopwatch();
+
+            for (var c = 0; c < 5; c++)
+            {
+                sw.Restart();
+                x.QuickSortSequential();
+                sw.Stop();
+                Console.WriteLine($"completed in {sw.ElapsedMilliseconds}ms");
+            }
+
+        }
+    }
+
+    public class QuickSortRunner
+    {
+        Action<Action<int[]>> run;
+
+        public int ItemRange { get; set; } = 1_000_000;
+
+        public QuickSortRunner()
+        {
+            Random rand = new Random((int) DateTime.Now.Ticks);
+
+            var dataSample = new int[ItemRange];
+            for (int i = 0; i < ItemRange; ++i)
+            {
+                dataSample[i] = rand.Next();
+            }
+
+            run = (sortFunc) =>
+            {
+                sortFunc(dataSample);
+            };
+        }
+
+        [Benchmark]
+        public void QuickSortSequential()
+        { 
+            run(QuickSort.QuickSort_Sequential);
+        }
+
+        [Benchmark]
+        public void QuickSortParallel()
+        { 
+            run(QuickSort.QuickSort_Parallel);
+        }
+
+        [Benchmark]
+        public void QuickSortParallelThreshold()
+        {
+            run(QuickSort.QuickSort_Parallel_Threshold);
+        }
+    }
+}
