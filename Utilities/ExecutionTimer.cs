3c53264433f13a971ece9543446a14f601916adc
Miguel <Miguel.Cudaihl@x-pressfeeders.com>
2018-08-30 14:08:04 +0800

    got to commit because I got to get back to work

---
+++ Utilities/ExecutionTimer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Utilities
+{
+    public class ExecutionTimer
+    {
+        Stopwatch _watch;
+        string _what;
+        object _context;
+
+        public ExecutionTimer(string activity, dynamic context = null)
+        {
+            _watch = Stopwatch.StartNew();
+            _what = activity;
+            _context = context;
+        }
+
+        public void Dispose()
+        {
+            _watch.Stop();
+           // Do something
+        }
+    }
+
+    public class ExecutionTimerFactory
+    {
+        public ExecutionTimer Create(string activity, dynamic context = null)
+        {
+            return new ExecutionTimer(activity, context);
+        }
+    }
+}
